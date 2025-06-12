9d4d720b37562501b7b95073409c7b3a5adde3f4
baizz <3024131925@qq.com>
2025-06-12 15:27:16 +0800

    重构(docs): 更新 Counter 组件，添加 TypeScript 支持并优化代码格式

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'no-shadow': 'off', // 允许变量同名，因为 ts 会检查
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-unused-vars': 'off', // 允许未使用的变量，因为 ts 会检查
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^' }],
+    'class-methods-use-this': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}; 
--- docs/.vitepress/components/Counter.vue
+++ docs/.vitepress/components/Counter.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-      <h3>这是一个 Vue Demo</h3>
-      <p>计数器: {{ count }}</p>
-      <button @click="increment">增加</button>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  const count = ref(0)
-  const increment = () => count.value++
-  </script>
-  
-  <style scoped>
-  button {
-    padding: 8px 16px;
-    background-color: #42b983;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  </style>
-  
+  <div>
+    <h3>这是一个 Vue Demo</h3>
+    <p>计数器: {{ count }}</p>
+    <button @click="increment">增加</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+const increment = () => count.value++;
+</script>
+
+<style scoped>
+button {
+  padding: 8px 16px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
